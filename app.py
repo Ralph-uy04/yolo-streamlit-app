731446055341dc214c241315064ee8d10c280b08
Ralph-uy04 <153211314+Ralph-uy04@users.noreply.github.com>
2025-05-16 21:40:52 +0800

    app.py

--- app.py
+++ app.py
@@ -13,8 +13,18 @@ biodegradable_items = [
 ]
 
 non_biodegradable_items = [
-    'bottle', 'cup', 'plastic bag', 'chair', 'laptop',
-    'cell phone', 'keyboard', 'toothbrush', 'scissors', 'teddy bear'
+    'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+    'stop sign', 'parking meter', 'bench', 'backpack', 'umbrella',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+    'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle',
+    'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'chair', 'couch', 'potted plant', 'bed', 'dining table',
+    'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+    'refrigerator', 'clock', 'vase', 'scissors',
+    'teddy bear', 'hair drier', 'toothbrush'
 ]
 
 # Page settings
